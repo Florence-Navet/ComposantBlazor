28444138809e33e8587550b2900fa06312613bb7
Florence Navet <enolaworld@gmail.com>
2025-11-30 17:49:33 +0100

    feat: create folder models and files personne, modified inputtext in the formulaire.razor

--- BlazorApp.WASM/Models/Personne.cs
+++ BlazorApp.WASM/Models/Personne.cs
@@ -2,10 +2,28 @@
 
 namespace BlazorApp.WASM.Models;
 
+public enum Sexe
+{
+    Masculin,
+    Feminin
+}
+
 public class Personne
 {
     //bien pour des validations basique 
     //[Required(AllowEmptyStrings =false, ErrorMessage ="Vous devez entrer votre nom !")]
     //[StringLength(256, MinimumLength = 3, ErrorMessage ="Nom entre 3 et 256 caractères")]
     public string? Nom { get; set; }
+
+    public string Bio { get; set; }
+
+    public int Age { get; set; }
+
+    public bool VIP { get; set; }   
+
+    public Sexe Sexe { get; set; }
+
+    public DateTime DateNaissance { get; set; }
+
+    public string Pays { get; set; }
 }
